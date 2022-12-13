189ef63890c47dad54fa7727a4f19828bb1f85d5
ASUS <ASUS@ISMAIL>
2022-12-13 23:21:16 +0300

    Add project files.

---
+++ Kods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Interface_2
+{
+    public class KullanıcıOperasyon
+    {
+        public void Oturumac() 
+        {
+            Console.WriteLine($"Oturum Açıldı...Tarih{DateTime.Now}");
+        }
+        public void OturumKapat() 
+        {
+            Console.WriteLine("Oturum Kapatıldı");
+        }
+
+    }
+
+    public interface IAdminOperasyon 
+    {
+        public void YıllıkSatısRaporlari() 
+        {
+            Console.WriteLine("Yıllık satis raporları gosteriliyor");
+        }
+        public void KendiSatislariniGoster(); // Admin kendisine bağlı üyeleri göstersin
+    }
+
+    public class AdminIslemleri : KullanıcıIslemleri, IAdminOperasyon 
+    {
+        public void KendiSatislariniGoster()
+        {
+            Console.WriteLine("Admin 100 adet ürün satti");
+        }
+    }
+
+    public class SuperAdminIslemleri : KullanıcıIslemleri, IAdminOperasyon
+    {
+        public void KendiSatislariniGoster() 
+        {
+            Console.WriteLine("SuperAdmin 50 adet ürün satti");
+        }
+    }
+
+    public class KullanıcıIslemleri : KullanıcıOperasyon
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _7_Interface_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KullanıcıIslemleri k = new KullanıcıIslemleri();
+            k.Oturumac();
+            k.OturumKapat();
+
+            AdminIslemleri a = new AdminIslemleri();
+            a.Oturumac();
+            a.KendiSatislariniGoster(); // interface den gelen üye
+            a.OturumKapat();
+
+            SuperAdminIslemleri s = new SuperAdminIslemleri();
+            a.Oturumac();  // interfaceden gelen üye
+            s.Oturumac();
+
+            IAdminOperasyon adminlereOzelOperasyon;
+
+            adminlereOzelOperasyon = a;
+            adminlereOzelOperasyon.KendiSatislariniGoster();
+            adminlereOzelOperasyon.YıllıkSatısRaporlari();
+
+            adminlereOzelOperasyon = s;
+            adminlereOzelOperasyon.KendiSatislariniGoster();
+            adminlereOzelOperasyon.YıllıkSatısRaporlari();
+
+            a.OturumKapat();
+            s.OturumKapat();
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
